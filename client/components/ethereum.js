e09cf919d6982e09b1c1629196340fc52af200d1
Tricia Lobo <tricia@Tricias-MacBook-Pro.local>
2018-10-01 14:14:20 -0400

    contract methods are working

--- client/components/ethereum.js
+++ client/components/ethereum.js
@@ -14,33 +14,54 @@ class Ethereum extends Component {
     }
   }
   async componentDidMount() {
+    console.log('factory', factory)
     const blocks = await factory.methods.getDeployedBlocks().call()
     console.log('blocks', blocks)
     let accounts = await web3.eth.getAccounts(console.log)
-    const createBlock = await factory.methods.createBlock(1).send({
-      gas: 1000000,
-      from: accounts[0]
-    })
-
-    const firstBlock = blocks[0]
-    const firstContract = fundsTransfer(firstBlock)
-    console.log('firstContract', firstContract) //has address
+    const createBlock = await factory.methods
+      .createBlock(1000000000000000000)
+      .send({
+        gas: 3000000,
+        from: accounts[0]
+      })
+    const lastBlockIndex = blocks.length - 1
+    const firstContract = fundsTransfer(blocks[lastBlockIndex])
+    console.log('first contract', firstContract)
+    const balanceInAccountOne = await web3.eth.getBalance(accounts[1])
+    console.log(
+      'balance in account 1 before depositing into contract',
+      balanceInAccountOne
+    )
     const depositFunds = await firstContract.methods.deposit().send({
-      gas: 1000000,
-      value: 1,
+      gas: 5000000,
+      value: 1000000000000000000,
       from: accounts[1]
     })
     const getBalance = await firstContract.methods.getBalance().call()
     console.log('balance in contract after deposit', getBalance)
-    // const withdrawFunds = await firstContract.methods
-    //   .withdraw(accounts[2])
-    //   .send({
-    //     gas: 1000000,
-    //     value: 1,
-
-    //   })
-    // const getBalanceTwo = await firstContract.methods.getBalance().call()
-    // console.log('balance in contract after withdraw', getBalanceTwo)
+    console.log(
+      'balance in account 1 after deposit',
+      await web3.eth.getBalance(accounts[1])
+    )
+    const balanceInAccountThreeBefore = await web3.eth.getBalance(accounts[3])
+    console.log(
+      'balance in account three before receiving funds',
+      balanceInAccountThreeBefore
+    )
+    const withdrawFunds = await firstContract.methods
+      .withdraw(accounts[3])
+      .send({
+        gas: 3000000,
+        from: accounts[1]
+      })
+    console.log(
+      'balance in account three after receiving funds',
+      await web3.eth.getBalance(accounts[3])
+    )
+    console.log(
+      'balance in account one after withdrawing funds',
+      await web3.eth.getBalance(accounts[1])
+    )
   }
 
   render() {
--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,6 +3,6 @@ import BlockFactory from './build/BlockFactory.json'
 
 const instance = new web3.eth.Contract(
   JSON.parse(BlockFactory.interface),
-  '0xd1b691E3DE533f72c25aD88a2aDf8296744BB738'
+  '0xb9A219631Aed55eBC3D998f17C3840B7eC39C0cc'
 )
 export default instance
