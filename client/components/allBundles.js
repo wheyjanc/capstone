45ffde83f519344385762d0ec5531029d8dd0b02
Stacy Harfenist <stacy.harfenist@gmail.com>
2018-10-05 13:19:23 -0400

    need Jan to style

--- client/components/allBundles.js
+++ client/components/allBundles.js
@@ -7,6 +7,11 @@ import { withStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+
 
 const styles = theme => ({
     root: {
@@ -14,15 +19,20 @@ const styles = theme => ({
       maxWidth: 360,
       backgroundColor: theme.palette.background.paper,
     },
+    nested: {
+        paddingLeft: theme.spacing.unit * 4,
+      },
   });
 
 class Bundles extends Component {
     state = {
         selectedIndex: 0,
+        open: false
       };
     
       handleListItemClick = (event, index, bundle) => {
         this.setState({ selectedIndex: index });
+        this.setState(state => ({open: !state.open}))
         this.props.setBundle(bundle)
         this.props.gotCampaignsInBundle(bundle.campaigns)
       };
@@ -40,20 +50,35 @@ class Bundles extends Component {
         return (
             this.props.bundles && this.props.bundles.length ?
           <div className={classes.root}>
-            <List component="nav">
+            <List component="nav" 
+            subheader = {<ListSubheader component = 'div'>Active Projects</ListSubheader>}>
             {this.props.bundles.map(bundle => {
                 const indexValue = index
                 index++
                 return (
+                    <div>
                     <ListItem
                     key = {bundle.id}
                     button
                     selected={this.state.selectedIndex === indexValue}
-                    // value = {bundle}
                     onClick={event => this.handleListItemClick(event, indexValue, bundle)}
                   >
                     <ListItemText primary={bundle.projectName} />
+                    {this.state.open? <ExpandLess /> : <ExpandMore />}
                   </ListItem> 
+                  <Collapse in={this.state.open} timeout = 'auto'>
+                    <List component = 'div' disabledPadding key = {bundle.id}>
+                        {bundle.campaigns.map(campaign => {
+                            return (
+                                <ListItem button className = {classes.nested} key = {campaign.id}>
+                                <ListItemText inset primary = {campaign.name} />
+                                </ListItem>
+                            )
+                        }
+                    )}
+                    </List>
+                    </Collapse>
+                    </div>
                 )
             })}
             </List>
