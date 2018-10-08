a523a30a5a697add5c55200ace529afc53a49469
Tricia Lobo <tricia@Tricias-MacBook-Pro.local>
2018-10-08 12:00:51 -0400

    etherium

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,6 +1,3 @@
-//import web3 from './web3'
-
-//import BlockFactory from './build/BlockFactory.json'
 const web3 = require('./web3')
 const BlockFactory = require('./build/BlockFactory.json')
 
@@ -9,4 +6,3 @@ const instance = new web3.eth.Contract(
   '0xb9A219631Aed55eBC3D998f17C3840B7eC39C0cc'
 )
 module.exports = instance
-//export default instance
